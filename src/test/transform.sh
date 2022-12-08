0981b003f2d8c952bf80086f46692faba0bd34c0
Toke Eskildsen <toes@kb.dk>
2022-12-08 11:42:45 +0100

    Add sample files for CPS

---
+++ src/test/transform.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+: ${INPUT:="$1"}
+: ${INPUT:="resources/xml/cps1/SKG_20160704_12_16_05.tif.xml"}
+
+: ${USE_ID:="$2"}
+: ${USE_ID:="ds.samlinger:3571e3f0-2ce6-11ed-81b4-005056882ec3"}
+
+: ${XSLT:="../main/resources/xslt/mods2solr.xsl"}
+
+: ${SAXON_JAR:="/home/$USER/saxon/saxon9he.jar"}
+SAXON="java -jar "$SAXON_JAR" --suppressXsltNamespaceCheck:on  "
+if [[ ! -s "$SAXON_JAR" ]]; then
+    >&2 echo "$SAXON_JAR not available. Please install it from https://sourceforge.net/projects/saxon/files/Saxon-HE/"
+    >&2 echo "The installation folder should be /home/$USER/saxon/"
+    exit 2
+fi
+for TOOL in jq; do
+    if [[ -z $(which $TOOL) ]]; then
+        >&2 echo "$TOOL not available. Please install it"
+        exit 3
+    fi
+done
+if [[ ! -s "$INPUT" ]]; then
+    >&2 echo "Input file not available: '$INPUT'"
+    echo ""
+    echo "Usage: ./transform.sh <inputfile>"
+    exit 2
+fi
+if [[ ! -s "$XSLT" ]]; then
+    >&2 echo "No XSLT '$XSLT'"
+    echo ""
+    echo "Usage: ./transform.sh <inputfile>"
+    exit 2
+fi
+use_id=''
+if [ $USE_ID ]; then
+    use_id="record_identifier=$USE_ID"
+fi
+
+# set to 1 if you want to prettyprint you json
+
+#json=$(sed 's/.xml$/.json/' <<< "$file")
+$SAXON -xsl:"$XSLT" -s:"$INPUT" $use_id | jq .
