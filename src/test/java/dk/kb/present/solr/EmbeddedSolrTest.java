c088ccb7a0e4b0c3960650aa563e2dcb72445765
Thomas Egense <teg@kb.dk>
2023-01-27 12:21:06 +0100

    Fixed relative dir for solr_home

--- src/test/java/dk/kb/present/solr/EmbeddedSolrTest.java
+++ src/test/java/dk/kb/present/solr/EmbeddedSolrTest.java
@@ -1,101 +1,101 @@
 package dk.kb.present.solr;
-import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest.METHOD;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrInputDocument;
+
 import org.apache.solr.core.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.HashMap;
-
-
-
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 public class EmbeddedSolrTest {
 
-    private static final Logger log = LoggerFactory.getLogger(EmbeddedSolrTest.class);        
-    private static String solr_home= "target/test-classes/solr"; 
-    //private static NetarchiveSolrClient server = null;
-    
-    private static CoreContainer coreContainer = null;
-    private static EmbeddedSolrServer embeddedServer = null;
-    
-    @BeforeAll
-    public static void setUp() throws Exception {
-        File solrHomeDir = new File("/home/teg/workspace/ds-present/target/test-classes/solr");
-        Path solrHome = solrHomeDir.toPath();
- 
-        System.setProperty( "solr.solr.home", solrHomeDir.getAbsolutePath() );
-        System.setProperty( "solr.install.dir", solrHomeDir.getAbsolutePath() );
-        
-        coreContainer  = new CoreContainer(solrHome, null );     
-        coreContainer.load();
-       embeddedServer = new EmbeddedSolrServer(coreContainer,"dssolr");
-      // NetarchiveSolrTestClient.initializeOverLoadUnitTest(embeddedServer);
-      // server = NetarchiveSolrClient.getInstance();
-       
-        // Remove any items from previous executions:
-  //     embeddedServer.deleteByQuery("*:*"); //This is not on the NetarchiveSolrClient API!
-    }
+	private static final Logger log = LoggerFactory.getLogger(EmbeddedSolrTest.class);
+	private static String solr_home = "target/test-classes/solr";
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @AfterAll
-    public static void tearDown() throws Exception {
-      coreContainer.shutdown(); 
-      embeddedServer.close();
-    }
-    
-  
-    @Test
-    public void testDateSortBug() throws Exception {
-    
-       String url = "http://testurl.dk/test";
-      
-       ArrayList<String> crawlTimes = new ArrayList<String>();
-       crawlTimes.add("2018-03-15T12:31:51Z");
-       crawlTimes.add("2018-03-15T12:34:37Z");
-       crawlTimes.add("2018-03-15T12:35:56Z");
-       crawlTimes.add("2018-03-15T12:36:14Z");
-       crawlTimes.add("2018-03-15T12:36:43Z"); //  <-- Excact match test #1
-       crawlTimes.add("2018-03-15T12:37:32Z");//   <-- nearest for test #3
-       crawlTimes.add("2018-03-15T12:37:52Z"); //  <-- nearest for test #2 
-       crawlTimes.add("2018-03-15T12:39:15Z");
-       crawlTimes.add("2018-03-15T12:40:09Z");
-             
-       int i =1;
-       for (String crawl : crawlTimes){
-         SolrInputDocument document = new SolrInputDocument();
-         String id = ""+i++; 
-                         
-         
-         document.addField("id", id);
-          
-         embeddedServer.add(document);
-         
-       }              
-       embeddedServer.commit();    
-       
-       
-    }
-    
+	private static CoreContainer coreContainer = null;
+	private static EmbeddedSolrServer embeddedServer = null;
+
+	@BeforeAll
+	public static void startEmbeddedSolrServer() throws Exception {
+	
+		File solrHomeDir = new File(solr_home);		
+		String solrHomeAbsoluteDir= solrHomeDir.getAbsolutePath();	
+		Path solrHome =  Paths.get(solrHomeAbsoluteDir);
+        System.setProperty("solr.install.dir", solrHomeAbsoluteDir);
+		Properties props = new Properties();
+		// props.put("solr.install.dir", solrHomeDir.getAbsolutePath()); //Does not
+		// work. Use system property above for now
+		coreContainer = new CoreContainer(solrHome, props);
+		coreContainer.load();
+		embeddedServer = new EmbeddedSolrServer(coreContainer, "dssolr");
+	}
+
+
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	public static void tearDown() throws Exception {
+		coreContainer.shutdown();
+		embeddedServer.close();
+	}
+
+	@BeforeEach
+	public void deleteDocs() throws Exception {
+		embeddedServer.deleteByQuery("*:*");
+	}
+
+	/*
+	 * Basic test. Add 10 documents with ID only
+	 * 
+	 */
+	@Test
+	public void testSolrServerIsRunning() throws Exception {
+
+		try {
+			for (int i = 0; i < 10; i++) {
+				SolrInputDocument document = new SolrInputDocument();
+				String id = "id" + i;
+				document.addField("id", id);
+				embeddedServer.add(document);
+
+			}
+			embeddedServer.commit();
+
+			// Test number of documents
+			SolrQuery solrQuery = new SolrQuery();
+			solrQuery.setQuery("*:*");
+			solrQuery.setRows(10);
+			solrQuery.add("fl", "id");
+
+			QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+
+			assertEquals(10L, rsp.getResults().getNumFound());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Error indexing 10 test documents only with id field");
+		}
+
+	}
 
 }
