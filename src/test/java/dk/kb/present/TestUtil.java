990a534aa65a8943f0e8f2497f5873498adfffda
Thomas Egense <teg@kb.dk>
2023-02-03 14:39:30 +0100

    remove system.out

--- src/test/java/dk/kb/present/TestUtil.java
+++ src/test/java/dk/kb/present/TestUtil.java
@@ -10,33 +10,31 @@ import dk.kb.util.Resolver;
 
 public class TestUtil {
 
-	
-	 public static String getTransformed(String xsltResource, String xmlResource) throws IOException {
-	        XSLTTransformer transformer = new XSLTTransformer(xsltResource);
-	        String mods = Resolver.resolveUTF8String(xmlResource);
-	        return transformer.apply(mods, new HashMap<String,String>());
-	    }	
-	 
-	 
-	  public static String getTransformed(String xsltResource, String xmlResource, Map<String,String> injections) throws IOException {
-	        XSLTTransformer transformer = new XSLTTransformer(xsltResource);
-	        String mods = Resolver.resolveUTF8String(xmlResource);
-	        if (injections == null) {
-	        	injections = new HashMap<String,String>();
-	        }        
-	        return transformer.apply(mods, injections);
-	    }
-	    
-	  
 
-		 public static String getTransformedWithAccessFieldsAdded(String xsltResource, String xmlResource) throws Exception {
-		        XSLTTransformer transformer = new XSLTTransformer(xsltResource);	       	        
-		   
-		        String mods = Resolver.resolveUTF8String(xmlResource);
-		        HashMap<String, String> accessFields = XsltCopyrightMapper.xsltCopyrightTransformer(mods);
-		        
-		        System.out.println("access fields:"+accessFields);
-		        return transformer.apply(mods, accessFields);
-		    }
-	 
+	public static String getTransformed(String xsltResource, String xmlResource) throws IOException {
+		XSLTTransformer transformer = new XSLTTransformer(xsltResource);
+		String mods = Resolver.resolveUTF8String(xmlResource);
+		return transformer.apply(mods, new HashMap<String,String>());
+	}	
+
+
+	public static String getTransformed(String xsltResource, String xmlResource, Map<String,String> injections) throws IOException {
+		XSLTTransformer transformer = new XSLTTransformer(xsltResource);
+		String mods = Resolver.resolveUTF8String(xmlResource);
+		if (injections == null) {
+			injections = new HashMap<String,String>();
+		}        
+		return transformer.apply(mods, injections);
+	}
+
+
+
+	public static String getTransformedWithAccessFieldsAdded(String xsltResource, String xmlResource) throws Exception {
+		XSLTTransformer transformer = new XSLTTransformer(xsltResource);	       	        		   
+		String mods = Resolver.resolveUTF8String(xmlResource);
+		HashMap<String, String> accessFields = XsltCopyrightMapper.xsltCopyrightTransformer(mods);		        
+		//System.out.println("access fields:"+accessFields);
+		return transformer.apply(mods, accessFields);
+	}
+
 }
