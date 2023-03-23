02260d3be5ac3f879d243674ac9eae78b287f76e
Victor Harbo Johnston <vhol@kb.dk>
2023-03-23 09:47:29 +0100

    add comment

--- src/test/java/dk/kb/present/transform/XSLTTransformerTest.java
+++ src/test/java/dk/kb/present/transform/XSLTTransformerTest.java
@@ -42,14 +42,14 @@ class XSLTTransformerTest {
     @Test
     void testJSONLDAlbert() throws IOException {
         JSONObject jsonld = new JSONObject( TestUtil.getTransformed(MODS2JSONLD, ALBERT));
-        // TODO: Remove this test as it is done on old metadata format
+        // TODO: When transformation to JSON-LD has been removed replace this test as it is done on old metadata format
         //assertTrue(jsonld.toString().contains("\"name\":{\"@value\":\"Einstein, Albert\",\"@language\":\"en\"}"));
     }
 
     @Test
     void testJSONLDChinese() throws IOException {
         JSONObject jsonld = new JSONObject(TestUtil.getTransformed(MODS2JSONLD, CHINESE));
-        // TODO: Remove this test as it is done on old metadata format
+        // TODO: When transformation to JSON-LD has been removed replace this test as it is done on old metadata format
         //assertTrue(jsonld.toString().contains("\"name\":{\"@value\":\"周培春 Zhou Peichun\",\"@language\":\"zh\"}"));
     }
 
