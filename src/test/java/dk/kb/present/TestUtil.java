4b3a869c9e57da78702c71570153770b70e81c54
Toke Eskildsen <toes@kb.dk>
2023-03-24 13:18:46 +0100

    Add test for creating an XSLTTransformer using XSLTFactory

--- src/test/java/dk/kb/present/TestUtil.java
+++ src/test/java/dk/kb/present/TestUtil.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import dk.kb.present.copyright.XsltCopyrightMapper;
+import dk.kb.present.transform.DSTransformer;
+import dk.kb.present.transform.XSLTFactory;
 import dk.kb.present.transform.XSLTTransformer;
 import dk.kb.util.Resolver;
+import dk.kb.util.yaml.YAML;
 
 public class TestUtil {
 
@@ -50,6 +53,21 @@ public class TestUtil {
 	public static String getTransformedWithAccessFieldsAdded(
             String xsltResource, String xmlResource, Map<String, String> injections) throws Exception {
 		XSLTTransformer transformer = new XSLTTransformer(xsltResource, injections);
+		String mods = Resolver.resolveUTF8String(xmlResource);
+		HashMap<String, String> accessFields = XsltCopyrightMapper.applyXsltCopyrightTransformer(mods);
+		//System.out.println("access fields:"+accessFields);
+		return transformer.apply(mods, accessFields);
+	}
+
+    /**
+     * Implicit test of {@link dk.kb.present.transform.XSLTFactory}.
+     * @param config XSLTFactory compliant YAML.
+     * @return the result generating a {@link dk.kb.present.transform.DSTransformer} using XSLTFactory and
+     *         transforming the {@code xmlResource} with that, including access fields resolved using
+     *         {@link XsltCopyrightMapper}.
+     */
+	public static String getTransformedFromConfigWithAccessFields(YAML config, String xmlResource) throws Exception {
+		DSTransformer transformer = new XSLTFactory().createTransformer(config);
 		String mods = Resolver.resolveUTF8String(xmlResource);
 		HashMap<String, String> accessFields = XsltCopyrightMapper.applyXsltCopyrightTransformer(mods);
 		//System.out.println("access fields:"+accessFields);
