367010b77987faf910964ae9e07fec78fdeba68e
Victor Harbo Johnston <vhol@kb.dk>
2023-09-05 07:52:48 +0200

    extract helper method

--- src/test/java/dk/kb/present/TestUtil.java
+++ src/test/java/dk/kb/present/TestUtil.java
@@ -84,12 +84,7 @@ public class TestUtil {
 						"  - imageserver: 'https://example.com/imageserver/'\n" +
 						"  - old_imageserver: 'http://kb-images.kb.dk'\n" +
 						"  - origin: 'ds.test'\n";
-		YAML yaml = YAML.parse(new ByteArrayInputStream(yamlStr.getBytes(StandardCharsets.UTF_8)));
-		String solrString = getTransformedFromConfigWithAccessFields(yaml, record);
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		JsonElement je = JsonParser.parseString(solrString);
-		String prettyJsonString = gson.toJson(je);
-		System.out.println(prettyJsonString);
+		prettyPrintSolrJson(record, yamlStr);
 	}
 
 	public static void prettyPrintSolrJsonFromPreservica(String record) throws Exception {
@@ -98,6 +93,10 @@ public class TestUtil {
 						"injections:\n" +
 						"  - streamingserver: 'example.com/streaming'\n" +
 						"  - origin: 'ds.test'\n";
+		prettyPrintSolrJson(record, yamlStr);
+	}
+
+	private static void prettyPrintSolrJson(String record, String yamlStr) throws Exception {
 		YAML yaml = YAML.parse(new ByteArrayInputStream(yamlStr.getBytes(StandardCharsets.UTF_8)));
 		String solrString = getTransformedFromConfigWithAccessFields(yaml, record);
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
