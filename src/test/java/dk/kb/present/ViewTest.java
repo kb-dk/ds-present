d4786fd694657abed0ae4889e432320f82d8ccdd
Victor Harbo Johnston <vhol@kb.dk>
2023-10-17 20:53:08 +0200

    remove test which has never run and isn't needed after new child delivery

--- src/test/java/dk/kb/present/ViewTest.java
+++ src/test/java/dk/kb/present/ViewTest.java
@@ -63,36 +63,4 @@ class ViewTest {
         assertTrue(solrJson.contains("\"thumbnail\":\"https:\\/\\/example.com\\/imageserver\\/%2FDAMJP2%2FDAM%2FSamlingsbilleder%2F0000%2F624%2F420%2FKE070592\\/full\\/%21150%2C150\\/0\\/default.jpg\"")
                 , "SolrJson does not contain correct thumbnail");
     }
-
-
-    //TODO: Fix this test by creating correct test config.
-    /*
-    @Test
-    void relationInView() throws Exception {
-        YAML conf = YAML.resolveLayeredConfigs("test_setup.yaml");
-        YAML radiotvConf = conf.getYAMLList(".config.collections").get(0);
-        View solrView = new View(radiotvConf.getSubMap("dsfl").getYAMLList("views").get(2), radiotvConf.getSubMap("dsfl").getString("origin"));
-
-        // Running the unit test locally. Elsewhere, the child uri should point to a ds-present record endpoint to get the raw value.
-        String pvicaRecord = Resolver.resolveUTF8String("internal_test_files/tvMetadata/e683b0b8-425b-45aa-be86-78ac2b4ef0ca.xml");
-        String localChildUri = String.valueOf(Resolver.getPathFromClasspath("internal_test_files/tvMetadata/53bf323c-5a8a-48b9-a29a-0b1616a58af9.xml"));
-
-        // Getting child remote
-        String childEndpoint = radiotvConf.getSubMap("dsfl").getString("getrecordendpoint");
-        String childId = URLEncoder.encode("ds.radiotv:oai:man:53bf323c-5a8a-48b9-a29a-0b1616a58af9", StandardCharsets.UTF_8);
-        String childURI = childEndpoint + childId + "?format=raw";
-
-
-        String solrJson = solrView.apply("40221e30-1414-11e9-8fb8-00505688346e", pvicaRecord, localChildUri);
-
-        Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
-        JsonElement je = JsonParser.parseString(solrJson);
-        String prettyJsonString = gson.toJson(je);
-        System.out.println(prettyJsonString);
-    }
-
-     */
-
-
-
 }
