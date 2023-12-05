ce39e7065f4aacb8bbcf43d6ca71581421c746df
Victor Harbo Johnston <vhol@kb.dk>
2023-12-05 06:54:56 +0100

    Update tests and remove the newest use of config.

--- src/test/java/dk/kb/present/ViewTest.java
+++ src/test/java/dk/kb/present/ViewTest.java
@@ -57,8 +57,9 @@ class ViewTest {
             return;
         }
         YAML conf = YAML.resolveLayeredConfigs("test_setup.yaml");
-        YAML radioConf = conf.getYAMLList(".origins").get(1);
-        View jsonldView = new View(radioConf.getSubMap("\"ds.radio\"").getYAMLList("views").get(1), radioConf.getSubMap("\"ds.radio\"").getString("origin"));
+        YAML radioConf = conf.getYAMLList("origins").get(2);
+        View jsonldView = new View(radioConf.getSubMap("\"ds.radio\"").getYAMLList("views").get(1),
+                                    radioConf.getSubMap("\"ds.radio\"").getString("origin"));
         String pvica = Resolver.resolveUTF8String(TestFiles.PVICA_RECORD_df3dc9cf);
 
         DsRecordDto recordDto = new DsRecordDto().data(pvica).id("test.id");
@@ -73,8 +74,9 @@ class ViewTest {
             return;
         }
         YAML conf = YAML.resolveLayeredConfigs("test_setup.yaml");
-        YAML radioConf = conf.getYAMLList(".config.origins").get(2);
-        View jsonldView = new View(radioConf.getSubMap("\"ds.radio\"").getYAMLList("views").get(1), radioConf.getSubMap("\"ds.radio\"").getString("origin"));
+        YAML radioConf = conf.getYAMLList(".origins").get(2);
+        View jsonldView = new View(radioConf.getSubMap("\"ds.radio\"").getYAMLList("views").get(1),
+                                    radioConf.getSubMap("\"ds.radio\"").getString("origin"));
         String pvica = Resolver.resolveUTF8String(TestFiles.PVICA_RECORD_df3dc9cf);
 
         DsRecordDto recordDto = new DsRecordDto().data(pvica).id("test.id");
@@ -92,14 +94,14 @@ class ViewTest {
             return;
         }
         YAML conf = YAML.resolveLayeredConfigs("test_setup.yaml");
-        YAML tvConf = conf.getYAMLList(".origins").get(1);
-        View solrView = new View(tvConf.getSubMap("\"ds.tv\"").getYAMLList("views").get(2), tvConf.getSubMap("\"ds.tv\"").getString("origin"));
+        YAML tvConf = conf.getYAMLList(".origins").get(3);
+        View solrView = new View(tvConf.getSubMap("\"ds.tv\"").getYAMLList("views").get(2),
+                                 tvConf.getSubMap("\"ds.tv\"").getString("origin"));
         String pvica = Resolver.resolveUTF8String("internal_test_files/tvMetadata/df3dc9cf-43f6-4a8a-8909-de8b0fb7bd00.xml");
 
         DsRecordDto recordDto = new DsRecordDto().data(pvica).id("test.id");
 
         String solrdoc = solrView.apply(recordDto);
-        System.out.println(solrdoc);
         assertTrue(solrdoc.contains("\"title\":\"Før Bjørnen Er Skudt\""));
     }
 
