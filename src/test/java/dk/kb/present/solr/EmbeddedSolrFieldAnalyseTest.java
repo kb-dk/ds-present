751e03e5ffe420f51fe15e5f2912104ac11af4fb
Thomas Egense <teg@kb.dk>
2024-02-09 09:13:29 +0100

    Reformattet file, killing all tabs.

--- src/test/java/dk/kb/present/solr/EmbeddedSolrFieldAnalyseTest.java
+++ src/test/java/dk/kb/present/solr/EmbeddedSolrFieldAnalyseTest.java
@@ -29,234 +29,215 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 public class EmbeddedSolrFieldAnalyseTest {
 
-	private static final Logger log = LoggerFactory.getLogger(EmbeddedSolrTest.class);
-	private static String solr_home = "target/solr";
+    private static final Logger log = LoggerFactory.getLogger(EmbeddedSolrTest.class);
+    private static String solr_home = "target/solr";
 
-	private static CoreContainer coreContainer = null;
-	private static EmbeddedSolrServer embeddedServer = null;
+    private static CoreContainer coreContainer = null;
+    private static EmbeddedSolrServer embeddedServer = null;
 
-	/*
-	 * Start Solr server and index test documents with method addTestDocuments
-	 * 
-	 */	
-	@BeforeAll
-	public static void startEmbeddedSolrServer() throws Exception {
+    /*
+     * Start Solr server and index test documents with method addTestDocuments
+     * 
+     */
+    @BeforeAll
+    public static void startEmbeddedSolrServer() throws Exception {
 
-		File solrHomeDir = new File(solr_home);		
-		String solrHomeAbsoluteDir= solrHomeDir.getAbsolutePath();	
-		Path solrHome =  Paths.get(solrHomeAbsoluteDir);
-		System.setProperty("solr.install.dir", solrHomeAbsoluteDir);
-		Properties props = new Properties();
-		// props.put("solr.install.dir", solrHomeDir.getAbsolutePath()); //Does not
-		// work. Use system property above for now
-		coreContainer = new CoreContainer(solrHome, props);
-		coreContainer.load();
-		embeddedServer = new EmbeddedSolrServer(coreContainer, "dssolr");
-	}
-
-	@AfterAll
-	public static void tearDown() throws Exception {
-		coreContainer.shutdown();
-		embeddedServer.close();
-	}
-	
-	
-	/*
-	 * Even diacritics must match for text_strict field 
-	 */
-	@Test
-    void testStrictTextField() throws Exception {
-		addSimpleFieldTestDocuments();
-		
-        assertEquals(0,getCreatorNameStrictResultsForQuery("Thomas XEgenseX")); //Make sure default operator is AND and not OR.
-        assertEquals(1,getCreatorNameStrictResultsForQuery("Thomas Egense"));
-        assertEquals(1,getCreatorNameStrictResultsForQuery("Thomas"));
-        assertEquals(1,getCreatorNameStrictResultsForQuery("thomas egense")); //lower case        
-        assertEquals(0,getCreatorNameStrictResultsForQuery("Thomas Gunter Grass")); //No match, different multivalue fields.
-        assertEquals(1,getCreatorNameStrictResultsForQuery("Antoine de Saint-Exupéry")); //Excact match
-        assertEquals(0,getCreatorNameStrictResultsForQuery("Antoine de Saint Exupéry")); //Even a character '-' must match
-        assertEquals(0,getCreatorNameStrictResultsForQuery("Antoine de Saint-Exupery")); //No match without diacritics
-    
+        File solrHomeDir = new File(solr_home);
+        String solrHomeAbsoluteDir = solrHomeDir.getAbsolutePath();
+        Path solrHome = Paths.get(solrHomeAbsoluteDir);
+        System.setProperty("solr.install.dir", solrHomeAbsoluteDir);
+        Properties props = new Properties();
+        // props.put("solr.install.dir", solrHomeDir.getAbsolutePath()); //Does not
+        // work. Use system property above for now
+        coreContainer = new CoreContainer(solrHome, props);
+        coreContainer.load();
+        embeddedServer = new EmbeddedSolrServer(coreContainer, "dssolr");
     }
-        
-        
-    
-	/*
-	 * Test normalized field match with and without diacriticts
-	 */
-	@Test
-	void testDiacriticsStripping() throws Exception {
-		addSimpleFieldTestDocuments();
-		// Test match with diacritic stripping in 'freetext' field
-		assertEquals(1,getFreeTextResultsForQuery("Thomas Grass")); // Combine names, should still match
-		//Diacritics
-		assertEquals(1,getFreeTextResultsForQuery("Thomas Egense"));
-		assertEquals(1,getFreeTextResultsForQuery("thomas egense")); //lower case
-		assertEquals(1,getFreeTextResultsForQuery("Antoine de Saint-Exupéry")); //with diacritics
-		assertEquals(1,getFreeTextResultsForQuery("Saint-Exupery")); //without diacritics
-		assertEquals(1,getFreeTextResultsForQuery("Saint Exupery")); // '-' removed
-		assertEquals(1,getFreeTextResultsForQuery("Honoré de Balzac")); 
-		assertEquals(1,getFreeTextResultsForQuery("Honore de Balzac"));		
-		assertEquals(1,getFreeTextResultsForQuery("Søren Kierkegaard"));
-		assertEquals(1,getFreeTextResultsForQuery("soren kierkegaard")); //Do we want OE match?		
-		assertEquals(1,getFreeTextResultsForQuery("Juliusz Słowacki"));
-		assertEquals(1,getFreeTextResultsForQuery("Juliusz Slowacki"));		
-		assertEquals(1,getFreeTextResultsForQuery("Maria Dąbrowska"));
-		assertEquals(1,getFreeTextResultsForQuery("maria dabrowska"));		
-		assertEquals(1,getFreeTextResultsForQuery("Gabriel García Márquez"));
-		assertEquals(1,getFreeTextResultsForQuery("Gabriel Garcia Marquez"));
-		assertEquals(1,getFreeTextResultsForQuery("Günter Grass"));
-		assertEquals(1,getFreeTextResultsForQuery("Gunter Grass"));
-		
-		
-		
-	}
 
-	@Test
-	public void testTitleStrict() throws SolrServerException, IOException {
-		addSimpleFieldTestDocuments();
-		assertEquals(1, getStrictTitleForQuery("\"Romeo og Julie\""));
-		assertEquals(1, getStrictTitleForQuery("\"Romeo and Juliet\""));
-		assertEquals(0, getStrictTitleForQuery("and"));
-		assertEquals(0, getStrictTitleForQuery("Julie"));
+    @AfterAll
+    public static void tearDown() throws Exception {
+        coreContainer.shutdown();
+        embeddedServer.close();
+    }
 
-	}
+    /*
+     * Even diacritics must match for text_strict field
+     */
+    @Test
+    void testStrictTextField() throws Exception {
+        addSimpleFieldTestDocuments();
 
-	@Test
-	public void testAnalysis() throws SolrServerException, IOException {
-		addSimpleFieldTestDocuments();
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("id:1");
-		solrQuery.setRows(10);
-		QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
-		//System.out.println(rsp.getResults().get(0).toString());
-	}
-	
-	@Test
-	public void testSynonymTest() throws SolrServerException, IOException {	
-		//Synonyms on the title field.  
-		addSynonymFieldTestDocuments();
-		assertEquals(1, getTitleQuery("tvavis").getNumFound());
-		assertEquals(1, getTitleQuery("\"tvavis\"").getNumFound()); // "tvavis" in quotes
-		assertEquals(1, getTitleQuery("tv-avisen").getNumFound()); //"tv-avisen" in quotes
-		assertEquals(1, getTitleQuery("\"tv-avisen\"").getNumFound());
-		assertEquals(1, getTitleQuery("tvavisen").getNumFound());												
-		assertEquals(1, getFreeTextQuery("tvavisen")); //Must also match here in the 'catch all field' Is indexed as 'tv-avisen'
-		
-		
-		//test title stored field is not replaced with synonyms
-		ArrayList<String> titles = (ArrayList<String>) getTitleQuery("\"tv-avisen\"").get(0).getFieldValue("title");		
-		assertEquals("Velkommen til tvavisen",titles.get(0));
-		
-	}
-	
-	
-	private long getCreatorNameStrictResultsForQuery(String query) throws Exception{	    		
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("creator_full_name_strict:("+query +")");
-		solrQuery.setRows(10);           
-		QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST); 		
-		return rsp.getResults().getNumFound();
-	}
+        assertEquals(0, getCreatorNameStrictResultsForQuery("Thomas XEgenseX")); // Make sure default operator is AND
+        // and not OR.
+        assertEquals(1, getCreatorNameStrictResultsForQuery("Thomas Egense"));
+        assertEquals(1, getCreatorNameStrictResultsForQuery("Thomas"));
+        assertEquals(1, getCreatorNameStrictResultsForQuery("thomas egense")); // lower case
+        assertEquals(0, getCreatorNameStrictResultsForQuery("Thomas Gunter Grass")); // No match, different multivalue
+        // fields.
+        assertEquals(1, getCreatorNameStrictResultsForQuery("Antoine de Saint-Exupéry")); // Excact match
+        assertEquals(0, getCreatorNameStrictResultsForQuery("Antoine de Saint Exupéry")); // Even a character '-' must
+        // match
+        assertEquals(0, getCreatorNameStrictResultsForQuery("Antoine de Saint-Exupery")); // No match without diacritics
 
-	
+    }
 
-	private long getFreeTextResultsForQuery(String query) throws Exception{	    		
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("freetext:("+query +")");
-		solrQuery.setRows(10);           
-		QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST); 		
-		return rsp.getResults().getNumFound();
-	}
+    /*
+     * Test normalized field match with and without diacriticts
+     */
+    @Test
+    void testDiacriticsStripping() throws Exception {
+        addSimpleFieldTestDocuments();
+        // Test match with diacritic stripping in 'freetext' field
+        assertEquals(1, getFreeTextResultsForQuery("Thomas Grass")); // Combine names, should still match
+        // Diacritics
+        assertEquals(1, getFreeTextResultsForQuery("Thomas Egense"));
+        assertEquals(1, getFreeTextResultsForQuery("thomas egense")); // lower case
+        assertEquals(1, getFreeTextResultsForQuery("Antoine de Saint-Exupéry")); // with diacritics
+        assertEquals(1, getFreeTextResultsForQuery("Saint-Exupery")); // without diacritics
+        assertEquals(1, getFreeTextResultsForQuery("Saint Exupery")); // '-' removed
+        assertEquals(1, getFreeTextResultsForQuery("Honoré de Balzac"));
+        assertEquals(1, getFreeTextResultsForQuery("Honore de Balzac"));
+        assertEquals(1, getFreeTextResultsForQuery("Søren Kierkegaard"));
+        assertEquals(1, getFreeTextResultsForQuery("soren kierkegaard")); // Do we want OE match?
+        assertEquals(1, getFreeTextResultsForQuery("Juliusz Słowacki"));
+        assertEquals(1, getFreeTextResultsForQuery("Juliusz Slowacki"));
+        assertEquals(1, getFreeTextResultsForQuery("Maria Dąbrowska"));
+        assertEquals(1, getFreeTextResultsForQuery("maria dabrowska"));
+        assertEquals(1, getFreeTextResultsForQuery("Gabriel García Márquez"));
+        assertEquals(1, getFreeTextResultsForQuery("Gabriel Garcia Marquez"));
+        assertEquals(1, getFreeTextResultsForQuery("Günter Grass"));
+        assertEquals(1, getFreeTextResultsForQuery("Gunter Grass"));
 
-	private long getStrictTitleForQuery(String query) throws SolrServerException, IOException {		
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("title_strict:"+query);
-		solrQuery.setRows(10);
-		QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
-		return rsp.getResults().getNumFound();
+    }
 
-	}
+    @Test
+    public void testTitleStrict() throws SolrServerException, IOException {
+        addSimpleFieldTestDocuments();
+        assertEquals(1, getStrictTitleForQuery("\"Romeo og Julie\""));
+        assertEquals(1, getStrictTitleForQuery("\"Romeo and Juliet\""));
+        assertEquals(0, getStrictTitleForQuery("and"));
+        assertEquals(0, getStrictTitleForQuery("Julie"));
 
+    }
 
-	private SolrDocumentList getTitleQuery(String query) throws SolrServerException, IOException {		
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("title:"+query);// Use edismax field defition which will also search in title_synonym
-		solrQuery.setRows(10);
-		QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
-		return rsp.getResults();
-	}
-	
-	private long getFreeTextQuery(String query) throws SolrServerException, IOException {		
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery(query);// Use edismax field defition which will also search in title_synonym
-		solrQuery.setRows(10);
-		QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
-		return rsp.getResults().getNumFound();
+    @Test
+    public void testAnalysis() throws SolrServerException, IOException {
+        addSimpleFieldTestDocuments();
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery("id:1");
+        solrQuery.setRows(10);
+        QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+        // System.out.println(rsp.getResults().get(0).toString());
+    }
 
-	}
-	
-	
-	private static void addSimpleFieldTestDocuments()  {
+    @Test
+    public void testSynonymTest() throws SolrServerException, IOException {
+        // Synonyms on the title field.
+        addSynonymFieldTestDocuments();
+        assertEquals(1, getTitleQuery("tvavis").getNumFound());
+        assertEquals(1, getTitleQuery("\"tvavis\"").getNumFound()); // "tvavis" in quotes
+        assertEquals(1, getTitleQuery("tv-avisen").getNumFound()); // "tv-avisen" in quotes
+        assertEquals(1, getTitleQuery("\"tv-avisen\"").getNumFound());
+        assertEquals(1, getTitleQuery("tvavisen").getNumFound());
+        assertEquals(1, getFreeTextQuery("tvavisen")); // Must also match here in the 'catch all field' Is indexed as
+        // 'tv-avisen'
 
-	try {
-				SolrInputDocument document = new SolrInputDocument();
-				document.addField("id", 1);
-				document.addField("origin", "ds.test");
-				document.addField("creator_full_name", "Thomas Egense");
-				document.addField("creator_full_name", "Antoine de Saint-Exupéry");
-				document.addField("creator_full_name", "Honoré de Balzac"); 
-				document.addField("creator_full_name", "Søren Kierkegaard");
-				document.addField("creator_full_name", "Juliusz Słowacki");
-				document.addField("creator_full_name", "Maria Dąbrowska");
-				document.addField("creator_full_name", "Gabriel García Márquez");
-				document.addField("creator_full_name", "Günter Grass");
-                document.addField("access_billede_aftale", false); //required field
-                document.addField("access_foto_aftale", false);//required field
-				document.addField("title", "Romeo og Julie");
-				document.addField("title", "Romeo and Juliet");
+        // test title stored field is not replaced with synonyms
+        ArrayList<String> titles = (ArrayList<String>) getTitleQuery("\"tv-avisen\"").get(0).getFieldValue("title");
+        assertEquals("Velkommen til tvavisen", titles.get(0));
 
-				embeddedServer.add(document);
-				embeddedServer.commit();
+    }
 
+    private long getCreatorNameStrictResultsForQuery(String query) throws Exception {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery("creator_full_name_strict:(" + query + ")");
+        solrQuery.setRows(10);
+        QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+        return rsp.getResults().getNumFound();
+    }
 
+    private long getFreeTextResultsForQuery(String query) throws Exception {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery("freetext:(" + query + ")");
+        solrQuery.setRows(10);
+        QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+        return rsp.getResults().getNumFound();
+    }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("Error indexing test documents");
-		}
+    private long getStrictTitleForQuery(String query) throws SolrServerException, IOException {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery("title_strict:" + query);
+        solrQuery.setRows(10);
+        QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+        return rsp.getResults().getNumFound();
 
-	}
+    }
 
-	
+    private SolrDocumentList getTitleQuery(String query) throws SolrServerException, IOException {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery("title:" + query);// Use edismax field defition which will also search in title_synonym
+        solrQuery.setRows(10);
+        QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+        return rsp.getResults();
+    }
 
-	
-	private static void addSynonymFieldTestDocuments()  {
+    private long getFreeTextQuery(String query) throws SolrServerException, IOException {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery(query);// Use edismax field defition which will also search in title_synonym
+        solrQuery.setRows(10);
+        QueryResponse rsp = embeddedServer.query(solrQuery, METHOD.POST);
+        return rsp.getResults().getNumFound();
 
-	try {
-				SolrInputDocument document = new SolrInputDocument();
-				document.addField("id", "synonym1");
-				document.addField("origin", "ds.test");
-				document.addField("title", "Velkommen til tvavisen");  //Synonym file: tv-avisen, tvavis, tvavisen, tv-avis => tv avisen
-				
-				embeddedServer.add(document);
-				embeddedServer.commit();
+    }
 
+    private static void addSimpleFieldTestDocuments() {
 
+        try {
+            SolrInputDocument document = new SolrInputDocument();
+            document.addField("id", 1);
+            document.addField("origin", "ds.test");
+            document.addField("creator_full_name", "Thomas Egense");
+            document.addField("creator_full_name", "Antoine de Saint-Exupéry");
+            document.addField("creator_full_name", "Honoré de Balzac");
+            document.addField("creator_full_name", "Søren Kierkegaard");
+            document.addField("creator_full_name", "Juliusz Słowacki");
+            document.addField("creator_full_name", "Maria Dąbrowska");
+            document.addField("creator_full_name", "Gabriel García Márquez");
+            document.addField("creator_full_name", "Günter Grass");
+            document.addField("access_billede_aftale", false); // required field
+            document.addField("access_foto_aftale", false);// required field
+            document.addField("title", "Romeo og Julie");
+            document.addField("title", "Romeo and Juliet");
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("Error indexing test documents");
-		}
+            embeddedServer.add(document);
+            embeddedServer.commit();
 
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error indexing test documents");
+        }
 
+    }
 
-	
+    private static void addSynonymFieldTestDocuments() {
 
+        try {
+            SolrInputDocument document = new SolrInputDocument();
+            document.addField("id", "synonym1");
+            document.addField("origin", "ds.test");
+            document.addField("title", "Velkommen til tvavisen"); // Synonym file: tv-avisen, tvavis, tvavisen, tv-avis
+            // => tv avisen
+
+            embeddedServer.add(document);
+            embeddedServer.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error indexing test documents");
+        }
+
+    }
 
 }
