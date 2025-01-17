cdecc8d73f5a14d65492cf55476c611c274ef323
Victor Harbo Johnston <vhol@kb.dk>
2025-01-17 12:42:47 +0100

    fix genre calculation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 - Fixed inclusion of the same dependencies from multiple sources.
+- Fixed that not all records did get a fallback genre.
 
 ## [2.2.5](https://github.com/kb-dk/ds-present/releases/tag/ds-present-2.2.5) 2025-01-06
 ### Added 
--- src/test/java/dk/kb/present/transform/XSLTPreservicaSchemaOrgTransformerTest.java
+++ src/test/java/dk/kb/present/transform/XSLTPreservicaSchemaOrgTransformerTest.java
@@ -560,4 +560,14 @@ public class XSLTPreservicaSchemaOrgTransformerTest extends XSLTTransformerTestB
         prettyPrintJson(transformedJSON);
     }
 
+    @Test
+    public void noGenreTest() throws IOException {
+        String transformedJson = TestUtil.getTransformedWithAccessFieldsAdded(PRESERVICA2SCHEMAORG, TestFiles.PVICA_RECORD_03f18f50);
+
+        prettyPrintJson(transformedJson);
+
+        assertTrue(transformedJson.contains("\"genre\":\"Radio-rodekasse\""));
+
+    }
+
 }
